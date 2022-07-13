7e4fe1843ea0ee53ceff237db1176106fa0f751b
Liz Roten <liz.roten@metc.state.mn.us>
2022-07-13 15:12:23 -0500

    no pandoc!

--- R/build.R
+++ R/build.R
@@ -1,3 +1,5 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
-# blogdown::build_dir("static")
+rmarkdown::render("static/slides/rstudioconf22-oddly-satisfying/slides.Rmd",
+                  "xaringan::moon_reader",
+                  run_pandoc = FALSE)
