6da6f4b02902a23624aeafea4c22ed9651ea943c
Liz Roten <elizabetheroten@gmail.com>
2022-09-01 11:25:07 -0500

    use git submodule sync on build; do not re-build slides

--- R/build.R
+++ R/build.R
@@ -1,7 +1,8 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
 
-system("git submodule foreach git pull origin main")
-rmarkdown::render("static/slides/rstudioconf22-oddly-satisfying/slides.Rmd",
-                  "xaringan::moon_reader",
-                  run_pandoc = FALSE)
+system("git submodule sync")
+
+# rmarkdown::render("static/slides/rstudioconf22-oddly-satisfying/slides.Rmd",
+#                   "xaringan::moon_reader",
+#                   run_pandoc = FALSE)
